0bb40d95a9183325adf1dad750bfe1b9dbd11a92
Carlos <carlos@mail.com>
2017-11-07 13:26:02 +0100

    Subida

---
+++ 02_modulos/modulos/src/app/shared/cabeza/cabeza.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cabeza',
+  templateUrl: './cabeza.component.html',
+  styleUrls: ['./cabeza.component.css']
+})
+export class CabezaComponent implements OnInit {
+  public title: string;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.title = 'Uso de Modulos';
+  }
+
+}
